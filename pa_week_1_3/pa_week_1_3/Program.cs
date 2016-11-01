5be88f4b3b43d4baa4547fa5a76f571f238bc366
s.zenkov <s.zenkov@corp.nekki.ru>
2016-11-01 18:56:20 +0300

    week 1, program assignment 1, task 3 preparings

---
+++ pa_week_1_3/pa_week_1_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pa_week_1_3
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
