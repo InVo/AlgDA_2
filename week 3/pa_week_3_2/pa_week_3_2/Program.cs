e0334c35318b30b11e8a2a79945ab0a7d98d1cae
InVo <szenckov@gmail.com>
2016-11-20 23:38:05 +0300

    week 3 task 2 complete solution, combined with task 1

--- week 3/pa_week_3_2/pa_week_3_2/Program.cs
+++ week 3/pa_week_3_2/pa_week_3_2/Program.cs
@@ -28,8 +28,8 @@ namespace pa_week_3_2
     class Program
     {
         static List<Item> _items;
-        static Dictionary<uint, long>[] _results;
-        static Dictionary<uint, long>[] _requests;
+        static long[] _curResults;
+        static long[] _prevResults;
 
         static void Main(string[] args)
         {
@@ -40,30 +40,49 @@ namespace pa_week_3_2
             int itemsCount = int.Parse(firstLineArgs[1]);
 
             _items = new List<Item>();
-            _results = new Dictionary<uint, long>[itemsCount];
-            _requests = new Dictionary<uint, long>[itemsCount];
+            _curResults = new long[totalCapacity + 1];
+            _prevResults = new long[totalCapacity + 1];
 
             for (int i = 1; i <= itemsCount; ++i)
             {
                 string[] arguments = lines[i].Split(' ');
-                uint weight = uint.Parse(arguments[0]);
-                uint value = uint.Parse(arguments[1]);
+                uint value = uint.Parse(arguments[0]);
+                uint weight = uint.Parse(arguments[1]);
                 var item = new Item(weight, value);
                 _items.Add(item);
-                _results[i - 1] = new Dictionary<uint, long>();
-                _requests[i - 1] = new Dictionary<uint, long>();
             }
 
             _items.Sort((a, b) =>
             {
-                return a.Weight.CompareTo(b.Weight);
+                if (a.Weight > b.Weight)
+                {
+                    return 1;
+                } else if (a.Weight == b.Weight)
+                {
+                    return 0;
+                }
+                return -1;
             });
 
-            long result = GetSolution(itemsCount - 1, totalCapacity);
+            for(int i = 0; i < itemsCount; ++i)
+            {
+                var item = _items[i];
+                for(uint j = item.Weight; j <= totalCapacity; ++j)
+                {
+                    _curResults[j] = Math.Max(_prevResults[j], _prevResults[j - item.Weight] + item.Value);
+                    long a = _curResults[j];
+                }
+                long[] temp = _prevResults;
+                _prevResults = _curResults;
+                _curResults = temp;
+                Array.Clear(_curResults, 0, (int)totalCapacity);
+            }
+
+            long result = _curResults[totalCapacity]; // Result big: 4243395, small: 2493893
             int c = 0;
         }
 
-        static long GetSolution(int topItem, uint  capacity)
+/*        static long GetSolution(int topItem, uint  capacity)
         {
             long result = 0;
             if (topItem < 0 || capacity <= 0)
@@ -81,6 +100,6 @@ namespace pa_week_3_2
             }
                 
             return result;
-        }
+        }*/
     }
 }
