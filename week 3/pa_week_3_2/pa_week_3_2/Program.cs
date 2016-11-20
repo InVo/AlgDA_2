5b68bde0969351f3ec7b3f7d5b3b0e880d2c9021
s.zenkov <s.zenkov@corp.nekki.ru>
2016-11-20 16:57:37 +0300

    week 3 task 2 update

--- week 3/pa_week_3_2/pa_week_3_2/Program.cs
+++ week 3/pa_week_3_2/pa_week_3_2/Program.cs
@@ -6,10 +6,80 @@ using System.Threading.Tasks;
 
 namespace pa_week_3_2
 {
+
+    class Item
+    {
+        public uint Weight;
+        public uint Value;
+
+        public Item(uint weight, uint value)
+        {
+            Weight = weight;
+            Value = value;
+        }
+    }
+
+    class ResultEntry
+    {
+        public long TopItem;
+        public long Capacity;
+
+        public override bool Equals(object obj)
+        {
+            ResultEntry other = obj as ResultEntry;
+            if (other != null)
+                return TopItem == other.TopItem && Capacity == other.Capacity;
+            else
+                return false;
+        }
+    }
+
     class Program
     {
+        static List<Item> _items;
+        static Dictionary<uint, long>[] _results;
+
         static void Main(string[] args)
         {
+            string[] lines = System.IO.File.ReadAllLines("knapsack_big.txt");
+            string[] firstLineArgs = lines[0].Split(' ');
+
+            uint totalCapacity = uint.Parse(firstLineArgs[0]);
+            int itemsCount = int.Parse(firstLineArgs[1]);
+
+            _items = new List<Item>();
+            _results = new Dictionary<uint, long>[itemsCount];
+
+            for(int i = 1; i <= itemsCount; ++i)
+            {
+                string[] arguments = lines[i].Split(' ');
+                uint weight = uint.Parse(arguments[0]);
+                uint value = uint.Parse(arguments[1]);
+                var item = new Item(weight, value);
+                _items.Add(item);
+                _results[i - 1] = new Dictionary<uint, long>();
+            }
+
+            long result = GetSolution(itemsCount - 1, totalCapacity);
+            int c = 0;
+        }
+
+        static long GetSolution(int topItem, uint  capacity)
+        {
+            long result = 0;
+            if (topItem < 0 || capacity <= 0)
+                return result;
+
+            if (_results[topItem].ContainsKey(capacity))
+            {
+                result = _results[topItem][capacity];
+            } else
+            {
+                result = Math.Max(GetSolution(topItem - 1, capacity),
+                    GetSolution(topItem - 1, capacity - _items[topItem].Weight) + _items[topItem].Value);
+                _results[topItem][capacity] = result;
+            }
+            return result;
         }
     }
 }
